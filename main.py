eb91a0590f6e1d40ac1bc8ad5608ca2072663e41
Mr-Quadratic <xdbaqueroiglesia5339@eagle.fgcu.edu>
2024-11-01 20:58:38 -0400

    Messing around with keyboard inputs and mixer. ASDF keys play A1, A2, A3, A4 respectively, but I don't know how to get sounds to play on top of each other, so each time you press a key the previous note just ends abruptly.

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ import sys
 import time
 
 from pygame import mixer
-
 pygame.mixer.init()
-sound = pygame.mixer.Sound('media/samples/UIO/C4.aiff')
-sound.play()
-time.sleep(10)
+pygame.init()
+
+display = pygame.display.set_mode((640, 480)) #Keyboard inputs won't register without a screen
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				mixer.music.load('media/samples/UIO/A1.aiff')
+				mixer.music.play()
+			if event.key == pygame.K_s:
+				mixer.music.load('media/samples/UIO/A2.aiff')
+				mixer.music.play()
+			if event.key == pygame.K_d:
+				mixer.music.load('media/samples/UIO/A3.aiff')
+				mixer.music.play()
+			if event.key == pygame.K_f:
+				mixer.music.load('media/samples/UIO/A4.aiff')
+				mixer.music.play()
