8fb10993b95f802724fc2f12f8bb646153ca770e
Mr-Quadratic <xdbaqueroiglesia5339@eagle.fgcu.edu>
2024-11-16 18:58:29 -0500

    Cleaned up a bit more
    Still need to fix channel limit and layer black keys above white

--- GUIstuff.py
+++ GUIstuff.py
@@ -4,25 +4,27 @@ from pygame_gui.core import ObjectID
 
 pygame.init()
 
+#Initialize GUI window
 pygame.display.set_caption('PYANO Dev GUI')
 window_surface = pygame.display.set_mode((1600, 800))
 background = pygame.Surface((1600, 600))
 background.fill(pygame.Color('#000000'))
 manager = pygame_gui.UIManager((1600, 1000), theme_path = 'theme.json')
 
-notes = ['C','D','E','F','G','A','B']
 
+
+#Lists for mapping notes to keys
+notes = ['C','D','E','F','G','A','B']
 flats_first = ['Db','Eb']
 flats_second = ['Gb','Ab','Bb']
-
 flat_position = [1,2]
-
 init_key_coord = 0
 init_flat_coord = 0
 octave = 1
 
 
-
+##Generate interactive piano key buttons
+#Generate white keys
 while octave <= 5:
 	for key in notes:
 		key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_key_coord, 650), (50, 150)),
@@ -31,11 +33,9 @@ while octave <= 5:
 		init_key_coord += 50
 	octave += 1
 
-
-
-
 black_keys = pygame.sprite.Group()
 octave = 1
+#Generate black keys (annoying because of the sort-of inconsistent spacing)
 while octave <= 5:
 	for pos in flat_position:
 		if pos == 1:
@@ -60,25 +60,3 @@ while octave <= 5:
 				black_keys.add(key)
 	octave += 1
 
-clock = pygame.time.Clock()
-is_running = True
-
-'''
-while is_running:
-	time_delta = clock.tick(60) / 1000.0
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			is_running = False
-
-		if event.type == pygame_gui.UI_BUTTON_PRESSED:
-			print(event.ui_element.text)
-
-		manager.process_events(event)
-
-	manager.update(time_delta)
-
-	window_surface.blit(background, (0, 0))
-	manager.draw_ui(window_surface)
-
-	pygame.display.update()
-'''
