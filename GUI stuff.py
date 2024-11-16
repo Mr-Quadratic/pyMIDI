6577d0ecf352d63c8b892b867e0dfb288b884f03
Mr-Quadratic <xdbaqueroiglesia5339@eagle.fgcu.edu>
2024-11-16 13:42:07 -0500

    UI button presses return the note of the key

--- GUI stuff.py
+++ GUI stuff.py
@@ -1,35 +1,41 @@
 import pygame
 import pygame_gui
+from pygame.examples.sprite_texture import group
 from pygame_gui.core import ObjectID
 
 pygame.init()
 
 pygame.display.set_caption('PYANO Dev GUI')
-window_surface = pygame.display.set_mode((1600, 600))
-
+window_surface = pygame.display.set_mode((1600, 800))
 background = pygame.Surface((1600, 600))
 background.fill(pygame.Color('#000000'))
-
-manager = pygame_gui.UIManager((1600, 600), theme_path = 'theme.json')
-
-buttons = ['C3','D3','E3','F3','G3','A3','B3','C4','D4','E4','F4','G4']
+manager = pygame_gui.UIManager((1600, 1000), theme_path = 'theme.json')
 
 notes = ['C','D','E','F','G','A','B']
 
 flats_first = ['Db','Eb']
 flats_second = ['Gb','Ab','Bb']
+
 flat_position = [1,2]
 
 init_key_coord = 0
 init_flat_coord = 0
 octave = 1
 
+
+
 while octave <= 5:
 	for key in notes:
-		key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_key_coord, 450), (50, 150)),text= f'{key}{octave}',manager=manager)
+		key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_key_coord, 650), (50, 150)),
+		                                   text= f'{key}{octave}',
+		                                   manager=manager)
 		init_key_coord += 50
 	octave += 1
 
+
+
+
+black_keys = pygame.sprite.Group()
 octave = 1
 while octave <= 5:
 	for pos in flat_position:
@@ -38,14 +44,21 @@ while octave <= 5:
 				init_flat_coord = 0
 				init_flat_coord += (37.5 + 50 * 7 * (octave-1))
 				init_flat_coord += (50 * flats_first.index(key))
-				key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_flat_coord, 450), (25, 100)), text = f'{key}{octave}', manager=manager,object_id=ObjectID(class_id='@black_button'))
+				key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_flat_coord, 650), (25, 100)),
+				                                   text = f'{key}{octave}',
+				                                   manager=manager,
+				                                   object_id=ObjectID(class_id='@black_button'))
+				black_keys.add(key)
 		if pos == 2:
 			for key in flats_second:
 				init_flat_coord = 0
 				init_flat_coord += (187.5 + 50 * 7 * (octave-1))
 				init_flat_coord += 50 * flats_second.index(key)
-				key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_flat_coord, 450), (25, 100)), text = f'{key}{octave}', manager=manager,object_id=ObjectID(class_id='@black_button'))
-
+				key = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((init_flat_coord, 650), (25, 100)),
+				                                   text = f'{key}{octave}',
+				                                   manager=manager,
+				                                   object_id=ObjectID(class_id='@black_button'))
+				black_keys.add(key)
 	octave += 1
 
 clock = pygame.time.Clock()
@@ -58,8 +71,7 @@ while is_running:
 			is_running = False
 
 		if event.type == pygame_gui.UI_BUTTON_PRESSED:
-			if event.ui_element in buttons:
-				print('This does nothing yet!')
+			print(event.ui_element.text)
 
 		manager.process_events(event)
 
